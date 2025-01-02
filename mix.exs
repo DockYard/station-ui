437e5404487df196a24d9de2359ae8eb52fa9b85
Miguel Camba <miguel.camba@gmail.com>
2025-01-02 14:52:37 +0100

    Update to phoenix 1.0

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule StationUI.MixProject do
     [
       {:gettext, "~> 0.20"},
       {:phoenix, "~> 1.7.11"},
-      {:phoenix_live_view, "~> 0.20.7"},
+      {:phoenix_live_view, "~> 1.0"},
       {:credo, "~> 1.7.10", only: [:dev, :test], runtime: false},
       {:tailwind_formatter, "~> 0.3.5", only: [:dev, :test], runtime: false}
     ]
